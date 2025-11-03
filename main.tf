4a28ba3016e0ee3570bfae26fb3664d1858de028
SharanaaSafiudeen <162840354+SharanaaSafiudeen@users.noreply.github.com>
2025-11-03 09:43:01 -0500

    Refactor AWS provider configuration in main.tf
    Updated Terraform configuration to use required_providers block.

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-
 # main.tf
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+ 
 }
-
-# Query a specific EC2 instance by ID
-data "aws_instance" "mywebserver" {
-  instance_id = "i-08467395fe27d70fa"
-}
-
-
-
